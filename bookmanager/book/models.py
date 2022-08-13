f51fd90d66de6aa5294ea500829f79a070b4a460
wesley <88048019@qq.com>
2022-08-13 09:41:00 +0000

    define model

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    #id The system will give to us id
+    name=models.CharField(max_length=10)
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    #foreign key
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
