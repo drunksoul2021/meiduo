30cf3a32c9a7f8d89435778da8e18a0a302d1edb
wesley <88048019@qq.com>
2022-08-14 02:37:43 +0000

    static resource setting and visit

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+#To tell system where is our images
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
